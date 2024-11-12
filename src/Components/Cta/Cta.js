eb8b3a1d80e7b3939bbf68394b833efcf50f43e6
emmanuelakharia <107748497+vibeauratech@users.noreply.github.com>
2024-11-12 17:17:35 +0100

    yes

--- src/Components/Cta/Cta.js
+++ src/Components/Cta/Cta.js
@@ -5,7 +5,7 @@ function Cta() {
   return (
     <div className='cta'>
        <div className='cta-con'>
-         <h2>Ready to experience the difference RadiantSkin can make to your skin?</h2>
+         <h2>Ready for that experience on your skin?</h2>
          <p>Shop at our collection online or visit any of our authorized retailers to discover skin care solutions that will leave your skin looking and feeling it's best.</p>
          <button>Shop Now</button>
        </div>
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,52 +7,51 @@ function Footer() {
   return (
     <div className='footer'>
       <div className='footer-con'>
-        <div>
+        <div className='footer-text'>
           <h2>RadiantSkin</h2>
-          <p></p>
+          <p>Become a part of our vibrant skincare community and discover the latest tips, trends and techniques for acheiving your best skin ever</p>
 
           <span>Sign up to our newsletter</span>
+
           <div>
             <input placeholder='Enter Email Address'></input>
             <button>Subscribe</button>
           </div>
-
         </div>
 
         <div className='footer-nav'>
-          <p></p>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+          <p>Company</p>
+          <span>About Us</span>
+          <span>Features</span>
+          <span>Products</span>
+          <span>Explore</span>
         </div>
 
-        <div>
-           <p></p>
-           <span></span>
+        <div className='footer-address'>
+           <p>Office Address</p>
+           <span>123 Blossom Lane, Sunrise Valley, CA 90210, United States.</span>
 
            <div className='socials'>
-             <p></p>
+             <p>Follow us on social media</p>
 
              <div className='social-con'>
               <div>
-                <img src='' alt=''></img>
+                <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1731398636/icons8-instagram-50_nmb6qg.png' alt=''></img>
               </div>
 
               <div>
-                <img src='' alt=''></img>
+                <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1731398636/icons8-twitter-50_sywqut.png' alt=''></img>
               </div>
 
               <div>
-                <img src='' alt=''></img>
+                <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1731398636/icons8-tik-tok-50_tujm1f.png' alt=''></img>
               </div>
               
               <div>
-                <img src='' alt=''></img>
+                <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1731398635/icons8-linkedin-50_pkh0a5.png' alt=''></img>
               </div>
              </div>
-           </div>
-          
+           </div>    
         </div>      
       </div>
 
